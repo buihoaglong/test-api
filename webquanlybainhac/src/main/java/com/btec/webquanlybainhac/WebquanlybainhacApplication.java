9143bcc7ac9ef4f2dfa2156b44ebbd99e71abfb7
Bùi Hoàng Long <MontestBHL@gmail.com>
2025-01-09 08:07:22 +0700

    commit file
    test API

---
+++ webquanlybainhac/src/main/java/com/btec/webquanlybainhac/WebquanlybainhacApplication.java
@@ -0,0 +1,13 @@
+package com.btec.webquanlybainhac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebquanlybainhacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebquanlybainhacApplication.class, args);
+	}
+
+}
---
+++ webquanlybainhac/src/test/java/com/btec/webquanlybainhac/WebquanlybainhacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btec.webquanlybainhac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebquanlybainhacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
